026afd23a4c63def56cd5cf9b933d73e11b5afd4
Jack Gilcrest <me@jp4g.llc>
2023-07-16 18:54:50 -0400

    leaf hash smoke tests

--- test/0.js
+++ test/0.js
@@ -10,7 +10,7 @@ describe("Test rollup", async () => {
         F = poseidon.F;
     });
 
-    it("should sign with poseidon", async () => {
+    xit("should sign with poseidon", async () => {
         // make account
         // let bytes = crypto.randomBytes(32);
         let bytes = Buffer.from("5049aa9160a5bcc3d80a60a3d3d5e40a106c3cec52583362f894fd4ca9c868f8", 'hex');
@@ -30,5 +30,19 @@ describe("Test rollup", async () => {
 
         // print values for verification
         console.log("signature", signature);
+    })
+
+    it("should produce hash for tx leaf", async () => {
+        // generate the expected tx leaf hash (used in noir test)
+        let message = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let messageHash = poseidon(message);
+        console.log("tx leaf hash", F.toObject(messageHash));
+    })
+
+    it("should produce hash for balance leaf", async () => {
+        // generate the expected balance leaf hash (used in noir test)
+        let message = [1, 2, 3, 4, 5]
+        let messageHash = poseidon(message);
+        console.log("balance leaf hash", F.toObject(messageHash));
     })
 })
