4ef6923ee3187dd0cabba93196d70a2fad9d22bb
Jack Gilcrest <me@jp4g.llc>
2023-07-16 21:32:19 -0400

    bb.js working

--- patch.sh
+++ patch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Patch script to get compatible version of bb.js
+echo "Building compatible bb.js from source"
+echo "Warning: this will take ~15-20 minutes"
 
 # Clone the barretenberg repository
 git clone https://github.com/AztecProtocol/barretenberg.git
@@ -8,10 +10,10 @@ git clone https://github.com/AztecProtocol/barretenberg.git
 # Build the barretenberg cpp library to wasm
 cd barretenberg/cpp
 ./bootstap.sh
-cmake --install build
 
 # Build the bb.js library
 cd ../ts
+yarn
 yarn build
 
 # move to node_js folder
--- test/0.js
+++ test/0.js
@@ -1,11 +1,13 @@
 const { buildEddsa, buildPoseidon, buildPedersenHash } = require('circomlibjs')
-import { SinglePedersen } from '@noir-lang/barretenberg/dest/crypto/pedersen';
+
+// import { SinglePedersen } from '@noir-lang/barretenberg/dest/crypto/pedersen';
 const crypto = require('crypto');
 
 describe("Test rollup", async () => {
-    let eddsa, poseidon, pedersen, F, zeroCache
+    let eddsa, poseidon, pedersen, F, zeroCache, bb
 
     before(async () => {
+        bb = await (await import("bb.js")).newBarretenbergApiAsync();
         eddsa = await buildEddsa();
         poseidon = await buildPoseidon();
         pedersen = await buildPedersenHash();
@@ -21,7 +23,7 @@ describe("Test rollup", async () => {
         // }
     });
 
-    it("should sign with poseidon", async () => {
+    xit("should sign with poseidon", async () => {
         // make account
         // let bytes = crypto.randomBytes(32);
         let bytes = Buffer.from("5049aa9160a5bcc3d80a60a3d3d5e40a106c3cec52583362f894fd4ca9c868f8", 'hex');
@@ -45,14 +47,14 @@ describe("Test rollup", async () => {
         console.log("signature", signature);
     })
 
-    it("should produce hash for tx leaf", async () => {
+    xit("should produce hash for tx leaf", async () => {
         // generate the expected tx leaf hash (used in noir test)
         let message = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         let messageHash = poseidon(message);
         console.log("tx leaf hash", F.toObject(messageHash));
     })
 
-    it("should produce hash for balance leaf", async () => {
+    xit("should produce hash for balance leaf", async () => {
         // generate the expected balance leaf hash (used in noir test)
         let message = [1, 2, 3, 4, 5]
         let messageHash = poseidon(message);
@@ -61,8 +63,8 @@ describe("Test rollup", async () => {
 
     it("pedersen smoke", async () => {
         let message = [1, 2, 3, 4, 5]
-        let messageHash = pedersen.hash(message);
-        console.log("pedersen hash", F.toObject(messageHash));
+        let messageHash = await bb.pedersenHashMultiple(message);
+        console.log("pedersen hash", messageHash);
     })
 
     // it("merkle tree", async () => {
